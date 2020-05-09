3bcfa17af837c72508756d8552b132e17b22f1b1
Fatai Balogun <fattylee.remod@gmail.com>
2020-05-09 18:20:12 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+import "./App.css";
+
+const App = () => {
+  const [val, setVal] = useState(4);
+  return <div>React running</div>;
+};
+
+export default App;
