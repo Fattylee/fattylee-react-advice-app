6197c6b1563f6472ff0e21cdd47a2c4207dee7b7
Fatai Balogun <fattylee.remod@gmail.com>
2020-05-18 15:05:19 +0100

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 
 import "./App.css";
 
 const App = () => {
   const [val, setVal] = useState(4);
-  return <div>React running</div>;
+  return (
+    <Fragment>
+      <div className="app">
+        <div className="card">
+          <h1>Default advice is here</h1>
+          <button>Give me advice</button>
+        </div>
+      </div>
+    </Fragment>
+  );
 };
 
 export default App;
