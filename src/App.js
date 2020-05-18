4de3cd4ec9f3cf9fd6e8d98e472d7f478d23d80f
Fatai Balogun <fattylee.remod@gmail.com>
2020-05-18 18:29:54 +0100

    version 1.0.0 completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 
 import "./App.css";
 
+const fetcAdvice = async (url) => {
+  const res = await fetch(url);
+  const resData = await res.json();
+  return resData.slip.advice;
+};
 const App = () => {
-  const [val, setVal] = useState(4);
+  const adviceUrl = "https://api.adviceslip.com/advice";
+  const [val, setVal] = useState("Default advice here");
+  useEffect(() => {
+    console.log("this act like componentDidMount to me");
+    fetcAdvice(adviceUrl).then((advice) => setVal(() => advice));
+  }, [adviceUrl]);
+
   return (
     <Fragment>
       <div className="app">
         <div className="card">
-          <h1>Default advice is here</h1>
-          <button>Give me advice</button>
+          <h1>{val}</h1>
+          <button
+            onClick={() => {
+              console.log("I got clicked!");
+              fetcAdvice(adviceUrl).then((advice) => setVal(() => advice));
+            }}
+          >
+            Give me advice
+          </button>
         </div>
       </div>
     </Fragment>
