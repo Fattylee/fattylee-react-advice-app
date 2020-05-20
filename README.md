9ddc247f3ed98af92579fb86caaf8d76fcfc897c
Fatai Balogun <fattylee.remod@gmail.com>
2020-05-20 21:48:48 +0100

    some test

--- README.md
+++
@@ -1,7 +0,0 @@
-# A slow pace into React Hooks
-
-## Get all the awesome advice from this app
-
-Live Demo -- Coming soon...
-
-Built with a create-react-appp boiler-plate
---
+++ precache-manifest.7cad5c9f5fbc0da29f91ca6f6d89d2e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d9c153d9939ba27efa44f8e2bcbff6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fde4effd8ff019941d4",
+    "url": "/static/css/main.c46f56a2.chunk.css"
+  },
+  {
+    "revision": "8be00c61a9001d17b246",
+    "url": "/static/js/2.0b460bc5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0b460bc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fde4effd8ff019941d4",
+    "url": "/static/js/main.eba19ce8.chunk.js"
+  },
+  {
+    "revision": "ab5278b2f8689c77f904",
+    "url": "/static/js/runtime-main.d7510bc7.js"
+  },
+  {
+    "revision": "48ed28f5b6e709bbb1b4f145b1db6c68",
+    "url": "/static/media/bg.48ed28f5.png"
+  }
+]);
