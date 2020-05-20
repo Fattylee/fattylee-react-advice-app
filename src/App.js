e7d08c907459b150b1b7e81afc37f3af63ce9cc6
Fatai Balogun <fattylee.remod@gmail.com>
2020-05-20 15:53:43 +0100

    complete version 1.0.1

--- src/App.js
+++ src/App.js
@@ -2,31 +2,86 @@ import React, { Fragment, useState, useEffect } from "react";
 
 import "./App.css";
 
-const fetcAdvice = async (url) => {
+const fetchAdvice = async (url) => {
   const res = await fetch(url);
   const resData = await res.json();
   return resData.slip.advice;
 };
 const App = () => {
   const adviceUrl = "https://api.adviceslip.com/advice";
-  const [val, setVal] = useState("Default advice here");
+  const [adviceState, setAdviceState] = useState({
+    advice: "Default advice here",
+    isLoading: false,
+  });
+
   useEffect(() => {
-    console.log("this act like componentDidMount to me");
-    fetcAdvice(adviceUrl).then((advice) => setVal(() => advice));
+    document.querySelector("title").innerHTML = "Advice App";
+    fetchAdvice(adviceUrl).then((advice) =>
+      setAdviceState((state) => ({ ...state, advice }))
+    );
   }, [adviceUrl]);
+
+  console.log(adviceState);
 
   return (
     <Fragment>
-      <div className="app">
-        <div className="card">
-          <h1>{val}</h1>
+      <div className={adviceState.error ? "modal-bg slide" : "modal-bg"}>
+        <div className={adviceState.error ? "modal slide" : "modal"}>
+          <p>{adviceState.errorMessage}</p>
           <button
             onClick={() => {
-              console.log("I got clicked!");
-              fetcAdvice(adviceUrl).then((advice) => setVal(() => advice));
+              setAdviceState((state) => ({
+                ...state,
+                error: false,
+              }));
             }}
           >
-            Give me advice
+            OK
+          </button>
+        </div>
+      </div>
+      <div className="app">
+        <div className="card">
+          <h1>{adviceState.advice}</h1>
+          <button
+            onClick={() => {
+              // trigger isLoading
+              setAdviceState((state) => ({ ...state, isLoading: true }));
+              clearTimeout(adviceState.errorId);
+              console.log("I got clicked!");
+              fetchAdvice(adviceUrl)
+                .then((advice) =>
+                  setAdviceState((state) => ({
+                    ...state,
+                    advice,
+                    isLoading: false,
+                  }))
+                )
+                .catch((err) => {
+                  const errorId = setTimeout(() => {
+                    setAdviceState((state) => ({
+                      ...state,
+                      error: false,
+                    }));
+                  }, 5000);
+
+                  if (err.message.includes("resource")) {
+                    err.message = err.message.replace("resource", "advice");
+                  }
+
+                  setAdviceState((state) => ({
+                    ...state,
+                    isLoading: false,
+                    error: true,
+                    errorMessage: err.message,
+                    errorId,
+                  }));
+                  console.log("no network, try again", err.message);
+                });
+            }}
+          >
+            Give me advice{" "}
+            <span className={adviceState.isLoading ? "loading" : ""}></span>
           </button>
         </div>
       </div>
