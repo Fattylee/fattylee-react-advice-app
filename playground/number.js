3208b3634b537aa5c3e125069fda4f0eb88736e1
Fatai Balogun <fattylee.remod@gmail.com>
2020-05-23 15:54:57 +0100

    save it for later, number.js

--- playground/number.js
+++ playground/number.js
@@ -15,3 +15,24 @@ console.log(num);
 
 num = number.toLocaleString("zh");
 console.log(num);
+
+let test = 40.9897237;
+// test = test | 0;
+test = Math.ceil(test);
+console.log(test);
+
+// console.log(
+// [...new Array(30)].map((item) => {
+// // console.log(5);
+// })
+// );
+
+const arr = [...new Array(4)];
+arr.push(67);
+// console.log(arr.length);
+// arr.forEach((item) => console.log(item));
+
+let value = encodeURIComponent(
+  (Math.floor(Math.random() * 123456) + 123456).toString()
+);
+console.log(value);
